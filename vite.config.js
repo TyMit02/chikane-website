12a06b0b844587799965810e57a11c9864690177
TyMit02 <tymitchell100@gmail.com>
2024-11-05 21:06:13 -0600

    idk what this is

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace with your repository name
+  base: '/chikane-website/', // Only for GitHub Pages deployment
   server: {
     port: 3000,
   },
