3100b8b1689068d5d894fccbb0270a979e3c9f55
TyMit02 <tymitchell100@gmail.com>
2024-11-08 14:34:26 -0600

    ajlsdk

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import DashboardLayout from '../components/dashboard/DashboardLayout';
+import DashboardHome from '../components/dashboard/DashboardHome';
 
 const Dashboard = () => {
-  return <DashboardLayout />;
+  return (
+    <DashboardLayout>
+      <Routes>
+        <Route index element={<DashboardHome />} />
+      </Routes>
+    </DashboardLayout>
+  );
 };
 
 export default Dashboard;
